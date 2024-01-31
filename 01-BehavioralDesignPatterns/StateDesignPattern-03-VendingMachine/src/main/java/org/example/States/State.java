8fe0b20013000072354a6e23f4443b99e87be77a
Suyash Mishra <suyyash25@gmail.com>
2024-01-31 13:08:37 +0530

    implement Vending Machine using StateDesign

---
+++ 01-BehavioralDesignPatterns/StateDesignPattern-03-VendingMachine/src/main/java/org/example/States/State.java
@@ -0,0 +1,27 @@
+package org.example.States;
+
+import org.example.Coin;
+import org.example.Item;
+import org.example.VendingMachine;
+
+import java.util.List;
+
+public interface State {
+     void clickOnInsertCoinButton(VendingMachine machine) throws Exception;
+
+     void clickOnStartProductSelectionButton(VendingMachine machine) throws Exception;
+
+     void insertCoin(VendingMachine machine , Coin coin) throws Exception;
+
+     void chooseProduct(VendingMachine machine, int codeNumber) throws Exception;
+
+     int getChange(int returnChangeMoney) throws Exception;
+
+     Item dispenseProduct(VendingMachine machine, int codeNumber) throws Exception;
+
+     List<Coin> refundFullMoney(VendingMachine machine) throws Exception;
+
+     void updateInventory(VendingMachine machine, Item item, int codeNumber) throws Exception;
+
+
+}
