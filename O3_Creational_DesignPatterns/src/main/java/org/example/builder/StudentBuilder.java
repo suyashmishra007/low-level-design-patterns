c6ce8d021f830d4145e82db70ca39c2d772d18e2
Suyash Mishra <suyyash25@gmail.com>
2024-03-04 22:46:21 +0530

    implement builder pattern for student

---
+++ O3_Creational_DesignPatterns/src/main/java/org/example/builder/StudentBuilder.java
@@ -0,0 +1,42 @@
+package org.example.builder;
+
+import java.util.*;
+
+public abstract class StudentBuilder {
+    int rollNumber;
+    int age ;
+    String name;
+    String fatherName;
+    String motherName;
+    List<String> subjects;
+
+    public StudentBuilder setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+        return this;
+    }
+
+    public StudentBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+    public StudentBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public StudentBuilder setFatherName(String fatherName) {
+        this.fatherName = fatherName;    return this;
+    }
+    public StudentBuilder setMotherName(String motherName) {
+        this.motherName = motherName;
+        return this;
+    }
+//    public StudentBuilder setSubjects(List<String> subjects) {
+//        this.subjects = subjects;
+//        return this;
+//    }
+
+    abstract public StudentBuilder setSubjects();
+    public Student build(){
+        return new Student(this);
+    }
+}
